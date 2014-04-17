ed27ad1099e51a1d5ce9161d40f1de358f19eb07
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-04-18 00:31:19 +0100

    Now with exercise 1.30

--- Chapter1/HigherOrderProcedures.rkt
+++ Chapter1/HigherOrderProcedures.rkt
@@ -1,8 +1,17 @@
-(define (sum term a next b)
+(define (sum-recursion term a next b)
   (if (> a b)
       0
       (+ (term a)
-         (sum term (next a) next b))))
+         (sum-recursion term (next a) next b))))
+
+;; Exercise 1.30
+
+(define (sum-iteration term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
 
 (define (inc n) (+ n 1))
 (define (identity x) x)
@@ -10,59 +19,60 @@
 
 ;; ********************************************************************************
 
-(define (sum-cubes a b)
+(define (sum-cubes sum a b)
   (sum cube a inc b))
 
-(sum-cubes 1 10)
+(sum-cubes sum-recursion 1 10)
+(sum-cubes sum-iteration 1 10)
 
 ;; ********************************************************************************
 
-(define (sum-integers a b)
+(define (sum-integers sum a b)
   (sum identity a inc b))
 
-(sum-integers 1 10)
+(sum-integers sum-recursion 1 10)
+(sum-integers sum-iteration 1 10)
 
 ;; ********************************************************************************
 
-(define (integral f a b dx)
+(define (integral sum f a b dx)
   (define (add-dx x) (+ x dx))
   (* (sum f (+ a (/ dx 2.0)) add-dx b)
      dx))
 
-(integral cube 0 1 0.01)
-(integral cube 0 1 0.001)
-(integral cube 0 1 0.0001)
-(integral cube 0 1 0.00001)
+(integral sum-recursion cube 0 1 0.01)
+(integral sum-recursion cube 0 1 0.001)
+(integral sum-recursion cube 0 1 0.0001)
+(integral sum-recursion cube 0 1 0.00001)
+
+(integral sum-iteration cube 0 1 0.01)
+(integral sum-iteration cube 0 1 0.001)
+(integral sum-iteration cube 0 1 0.0001)
+(integral sum-iteration cube 0 1 0.00001)
 
 ;; ********************************************************************************
 
 ;; Exercise 1.29
 
-(define (integral-using-simpsons-rule f a b n)
-  (define (helper h)
-    (define (term k)
-      (* (f (+ a (* k h)))
-         (cond
-           ((= k 0) 1)
-           ((= k n) 1)
-           ((even? k) 2)
-           (else 4))))
-    (* (sum term 0 inc n)
-       (/ h 3)))
-  (helper (/ (- b a) n)))
+(define (integral-using-simpsons-rule sum f a b n)
+  (define h (/ (- b a) n))
+  (define (term k)
+    (* (f (+ a (* k h)))
+       (cond
+         ((= k 0) 1)
+         ((= k n) 1)
+         ((even? k) 2)
+         (else 4))))
+  (* (sum term 0 inc n)
+     (/ h 3)))
 
-(integral-using-simpsons-rule cube 0.0 1.0 10)
-(integral-using-simpsons-rule cube 0.0 1.0 100)
-(integral-using-simpsons-rule cube 0.0 1.0 1000)
+(integral-using-simpsons-rule sum-recursion cube 0.0 1.0 10)
+(integral-using-simpsons-rule sum-recursion cube 0.0 1.0 100)
+(integral-using-simpsons-rule sum-recursion cube 0.0 1.0 1000)
+
+(integral-using-simpsons-rule sum-iteration cube 0.0 1.0 10)
+(integral-using-simpsons-rule sum-iteration cube 0.0 1.0 100)
+(integral-using-simpsons-rule sum-iteration cube 0.0 1.0 1000)
 
 ;; ********************************************************************************
-
-;; Exercise 1.30
-
-(define (sum-iter term a next b)
-  (define (iter a result)
-    (if (> a b)
-        0
-        (iter ?? ??)))
-    (iter a 0))
 
