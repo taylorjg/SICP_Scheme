42ba3b0274e2b49de2a82f93f6cb0bff753ef80e
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-04-21 16:57:51 +0100

    Done exercise 2.3

--- Chapter2/PointLineRect.rkt
+++ Chapter2/PointLineRect.rkt
@@ -46,3 +46,39 @@
 (print-point (midpoint-segment s))
 
 ;; ********************************************************************************
+
+;; Exercise 2.3
+
+; 1st representation of a rectangle: using two points
+;(define (make-rect p1 p2)
+;  (cons p1 p2))
+;
+;(define (rect-width r)
+;  (abs (- (x-point (car r)) (x-point (cdr r)))))
+;
+;(define (rect-height r)
+;  (abs (- (y-point (car r)) (y-point (cdr r)))))
+
+; 2nd representation of a rectangle: using centre point + width + height
+(define (make-rect centre-point w h)
+  (cons centre-point (cons w h)))
+
+(define (rect-width r)
+  (car (cdr r)))
+
+(define (rect-height r)
+  (cdr (cdr r)))
+
+; rect-area and rect-perimeter do not need to change when swapping
+; between the two different representations of a rectangle.
+
+(define (rect-area r)
+  (* (rect-width r) (rect-height r)))
+
+(define (rect-perimeter r)
+  (+ (* 2 (rect-width r))
+     (* 2 (rect-height r))))
+
+;(define r1 (make-rect (make-point 0 0) (make-point 5 10)))
+(define r2 (make-rect (make-point 2.5 5) 5 10))
+
