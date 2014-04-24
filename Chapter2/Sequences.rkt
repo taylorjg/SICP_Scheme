9d3ec29b118e02d1e5e2086d99d875faf0a1d437
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-04-24 13:35:05 +0100

    Done exercise 2.21

--- Chapter2/Sequences.rkt
+++ Chapter2/Sequences.rkt
@@ -54,3 +54,26 @@
 
 ;; ********************************************************************************
 
+;; Exercise 2.21
+
+(define (square x) (* x x))
+
+(define (square-list-1 items)
+  (if (null? items)
+      nil
+      (cons (square (car items)) (square-list-1 (cdr items)))))
+
+(define (map-jt f items)
+  (if (null? items)
+      nil
+      (cons (f (car items))
+            (map-jt f (cdr items)))))
+
+(define (square-list-2 items)
+  (map-jt square items))
+
+(square-list-1 (list 1 2 3 4))
+(square-list-2 (list 1 2 3 4))
+
+;; ********************************************************************************
+
