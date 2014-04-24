3016f9f3aa875b12f8cb3c896ab84e8ea0ad0acd
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-04-24 22:59:49 +0100

    Done exercises 2.26 & 2.27

--- Chapter2/Sequences.rkt
+++ Chapter2/Sequences.rkt
@@ -121,3 +121,46 @@
 
 ;; ********************************************************************************
 
+;; Exercise 2.26
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(newline)
+(append x y)
+; (1 2 3 4 5 6)
+
+(newline)
+; (cons x y)
+; ((1 2 3) 4 5 6)
+
+(newline)
+(list x y)
+; ((1 2 3) (4 5 6))
+
+;; ********************************************************************************
+
+;; Exercise 2.27
+
+(define (deep-reverse items)
+  (define (iter a b)
+    (if (null? a)
+        b
+        (iter (cdr a) (cons (if (pair? (car a))
+                                (deep-reverse (car a))
+                                (car a)) b))))
+  (iter items nil))
+
+(define deep-list (list (list 1 2) (list 3 4)))
+
+(newline)
+(display deep-list)
+
+(newline)
+(reverse deep-list)
+
+(newline)
+(deep-reverse deep-list)
+
+;; ********************************************************************************
+
