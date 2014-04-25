ed65acedd0fdb2abcbfbd6abfdd9982c875d4ed2
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-04-25 22:13:21 +0100

    Done exercise 2.32

--- Chapter2/Sequences.rkt
+++ Chapter2/Sequences.rkt
@@ -221,4 +221,14 @@
 
 ;; Exercise 2.32
 
+(define (subsets s)
+  (if (null? s)
+      (list nil)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map-jt
+                      (lambda (x) (cons (car s) x))
+                      rest)))))
+
+(subsets (list 1 2 3))
+
 ;; ********************************************************************************
