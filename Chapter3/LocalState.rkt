27ea0acb10871e40f7bfd81777499c4016e33eed
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-05-02 13:36:47 +0100

    Done exercise 3.2

--- Chapter3/LocalState.rkt
+++ Chapter3/LocalState.rkt
@@ -12,3 +12,27 @@
 (A 10)
 
 ;; --------------------------------------------------------------------------------
+
+;; Exercise 3.2
+
+(define (make-monitored f)
+  (let ((count 0))
+    (define (dispatch m)
+      (cond ((eq? m 'how-many-calls?) count)
+            ((eq? m 'reset-count) (set! count 0))
+            (else (begin (set! count (+ count 1)) (f m))))
+      )
+    dispatch)
+  )
+
+(define mf (make-monitored sqrt))
+(mf 100)
+(mf 'how-many-calls?)
+(mf 49)
+(mf 'how-many-calls?)
+(mf 'reset-count)
+(mf 'how-many-calls?)
+(mf 81)
+(mf 'how-many-calls?)
+
+;; --------------------------------------------------------------------------------
